f0990024f0f757710de3939347f01dc0cbf13d96
author-sanjay <sanjaykumar73189@gmail.com>
2020-07-12 20:27:31 +0530

    hacker rank question 5 table added

--- practice.java
+++ practice.java
@@ -68,3 +68,34 @@ public class Solution {
 
 
 
+===============================================================================================================================================================================
+
+
+Question: Print table of a given number in JAVA till 10.
+Answer:
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        //scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        for(int i=1; i<=10;i++){
+            System.out.println(N+" x "+i+" = "+N*i);
+        }
+
+        scanner.close();
+    }
+}
